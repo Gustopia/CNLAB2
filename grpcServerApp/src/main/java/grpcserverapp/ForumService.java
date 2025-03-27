dd3b5d96710952335ce5597fd035e38ff1d005ec
Gustavo <153638647+Gustopia@users.noreply.github.com>
2025-03-27 10:46:22 +0000

    as

--- grpcServerApp/src/main/java/grpcserverapp/ForumService.java
+++ grpcServerApp/src/main/java/grpcserverapp/ForumService.java
@@ -1,5 +1,14 @@
 package grpcserverapp;
 
-public class ForumService {
-    dd
+import io.grpc.Status;
+import io.grpc.StatusException;
+import io.grpc.stub.StreamObserver;
+import servicestubs.*;
+
+public class ForumService extends ServiceGrpc.ServiceImplBase{
+
+    public ForumService(int svcPort){
+
+    }
+
 }
